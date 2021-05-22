3195e16cc375edb2b2f7a84d7f2b572568d434a7
adias9 <andreasdias9@gmail.com>
2021-05-22 17:15:55 -0500

    display messages and create Message component

--- index.js
+++ index.js
@@ -1,14 +1,34 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { render } from 'react-dom';
 import './style.css';
+
+// External Components
+import Message from './components/Message';
 
 // This is the list of messages.
 import { messages } from './data.json';
 
-class App extends Component {
-  render() {
-    return <p>123</p>;
-  }
+function App() {
+  console.log('messages: ', messages);
+  const filteredMessages = messages.filter(
+    (m, idx, self) =>
+      idx ===
+      self.findIndex(
+        t =>
+          (t.content === m.content && t.senderUuid === m.senderUuid) ||
+          (t.content === m.content && t.uuid === m.uuid)
+      )
+  );
+  console.log('filteredMessages: ', filteredMessages);
+
+  return filteredMessages.map(m => (
+    <Message
+      content={m.content}
+      senderUuid={m.senderUuid}
+      sentAt={m.sentAt}
+      uuid={m.uuid}
+    />
+  ));
 }
 
 render(<App />, document.getElementById('root'));

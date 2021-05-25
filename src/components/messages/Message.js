83cbcd41e7c41e43e5b9ad5aa25fb1b036c06be7
Andreas Dias <andreas.d@taboola.com>
2021-05-24 23:24:31 -0500

    update date to human readable format

--- src/components/messages/Message.js
+++ src/components/messages/Message.js
@@ -7,12 +7,13 @@ import { faTrash } from '@fortawesome/free-solid-svg-icons';
 import { CURR_USER_ID } from '../../constants';
 
 export default function Message({ content, senderUuid, sentAt, uuid, handleDeleteButton }) {
-  let formattedDate = new Date(sentAt);
+  const dateOptions = { year: "numeric", month: "long", day: "numeric", hour: "numeric", minute: "numeric" }
+  let formattedDate = new Date(sentAt).toLocaleDateString(undefined, dateOptions);
   let messageType = (senderUuid === CURR_USER_ID) ? 'sender' : 'recipient';
   return (
     <div className={`chat-message ${messageType}`}>
       <p className={`message-content ${messageType}`}>{content}</p>
-      <p className="message-timestamp">{formattedDate.toLocaleString()}</p>
+      <p className="message-timestamp">{formattedDate}</p>
       <div className="message-delete">
           <span onClick={()=>handleDeleteButton({uuid: uuid, sentAt: sentAt})}><FontAwesomeIcon icon={faTrash} /></span>
       </div>
